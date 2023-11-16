baea17992b9305f1f2dac141c8f80cfd9fa1e6b6
Naidru <joeypatten8@gmail.com>
2023-11-16 10:19:58 -0400

    Update and rename installmenu.bat to installmenu.ps1

---
+++ installmenu.ps1
@@ -0,0 +1,28 @@
+$title = "Lethal Company - Modded Installation Kit - Created by Naidru"
+Write-Host "What is this program?"
+Write-Host "====================="
+Write-Host "This is a Powershell script that contains a compilation of certain modpacks that are easy to install with 1 press of a button."
+Write-Host "It will automatically download from the internet a selection of mods."
+Write-Host ""
+Write-Host "This program is based off of 'Krystilize's' BiggerLobby installer"
+Write-Host ""
+Write-Host ""
+Write-Host "Created by Naidru"
+Pause
+Clear-Host
+:menu
+Write-Host "Please select a modpack"
+Write-Host ""
+Write-Host "1 - BiggerLobby Mod"
+Write-Host "2 - Quit"
+$selection = Read-Host "Enter your selection"
+if ($selection -eq 1) {
+    iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Naidru/LethalCompanyInstaller/main/modpacks/BiggerLobby/BiggerLobby.bat'))
+}
+elseif ($selection -eq 2) {
+    exit
+}
+else {
+    Write-Host "An invalid option was selected!"
+    goto menu
+}

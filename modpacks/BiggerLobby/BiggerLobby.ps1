179fc9a014305de0a2dd9ccb14566e2641319807
Naidru <joeypatten8@gmail.com>
2023-11-16 10:32:01 -0400

    Update BiggerLobby.ps1

--- modpacks/BiggerLobby/BiggerLobby.ps1
+++ modpacks/BiggerLobby/BiggerLobby.ps1
@@ -4,22 +4,24 @@ Write-Host "BiggerLobby"
 Write-Host "-----------"
 Write-Host "Allows lethal company lobbies to have up to 20 players!"
 Write-Host ""
-Write-Host "Please select a version"
-Write-Host ""
-Write-Host "1 - Stable Release"
-Write-Host "2 - Beta Release"
-Write-Host "3 - Quit"
-$modver = Read-Host "Enter your selection"
-if ($modver -eq 1) {
-    iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Naidru/LethalCompanyInstaller/main/modpacks/BiggerLobby/install-mod.ps1'))
-}
-elseif ($modver -eq 2) {
-    iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Naidru/LethalCompanyInstaller/main/modpacks/BiggerLobby/install-modBETA.ps1'))
-}
-elseif ($modver -eq 3) {
-    exit
-}
-else {
-    Write-Host "Invalid Option!"
-    goto biggerlobby
+while ($true) {
+    Write-Host "Please select a version"
+    Write-Host ""
+    Write-Host "1 - Stable Release"
+    Write-Host "2 - Beta Release"
+    Write-Host "3 - Quit"
+    $modver = Read-Host "Enter your selection"
+    if ($modver -eq 1) {
+        iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Naidru/LethalCompanyInstaller/main/modpacks/BiggerLobby/install-mod.ps1'))
+    }
+    elseif ($modver -eq 2) {
+        iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Naidru/LethalCompanyInstaller/main/modpacks/BiggerLobby/install-modBETA.ps1'))
+    }
+    elseif ($modver -eq 3) {
+        exit
+    }
+    else {
+        Write-Host "Invalid Option!"
+        goto biggerlobby
+    }
 }
